f589062e2ab90e0539be75419921b1c7354697fb
Adrian Meza <adrian.meza@parivedasolutions.com>
2020-05-26 13:13:42 -0500

    complete statements and arrays course (@adrianme213)

--- Java. Statements and Arrays/Multi-dimensional array/Programming (8)/src/Main.java
+++ Java. Statements and Arrays/Multi-dimensional array/Programming (8)/src/Main.java
@@ -1,22 +1,55 @@
 import java.util.Scanner;
 
 public class Main {
+  static int val = 1;
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
-    int[][] matrix = new int[n][n];
-    int nsquared = n*n;
 
-    for(int i = 0;i < n;i++){
-      for(int j = 0;j < n;j++){
-        matrix[i][j] = scanner.nextInt();
-      }
+    int[][] array = new int[n][n];
+    int row = n;
+    int col = n;
+
+    int initRow = 0;
+    int initCol = 0;
+
+    while (val < n * n + 1) {
+      leftToRightFill(array, initRow, initCol, col);
+      initRow++;
+      topToDownFill(array, initRow, row, col);
+      col--;
+      rightToLeftFill(array, row - 1, initCol, col);
+      row--;
+      downToTopFill(array, initRow, row, initCol);
+      initCol++;
     }
 
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        System.out.print(array[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
 
+  private static void leftToRightFill(int[][] array, int row, int initCol, int col) {
+    for (int j = initCol; j < col; j++)
+      array[row][j] = val++;
+  }
 
-    System.out.println(result);
-    scanner.close();
+  private static void topToDownFill(int[][] array, int initRow, int row, int col) {
+    for (int i = initRow; i < row; i++)
+      array[i][col - 1] = val++;
+  }
+
+  private static void rightToLeftFill(int[][] array, int row, int initCol, int col) {
+    for (int j = col - 1; j >= initCol; j--)
+      array[row][j] = val++;
+  }
+
+  private static void downToTopFill(int[][] array, int initRow, int row, int col) {
+    for (int j = row - 1; j >= initRow; j--)
+      array[j][col] = val++;
   }
 }
--- Java. Statements and Arrays/Multi-dimensional array/Programming (9)/src/Main.java
+++ Java. Statements and Arrays/Multi-dimensional array/Programming (9)/src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
     //put your code here
+    Scanner scanner = new Scanner(System.in);
+    int[][] a = new int[100][100];
+    String s;
+    String[] c;
+    int m=0,n=0;
+    while(scanner.hasNextInt()){
+      s=scanner.nextLine();
+      c=s.split(" ");
+      m++; if(n==0) n=c.length;
+      for(int i=1;i<=c.length;i++) a[m][i] = Integer.parseInt(c[i-1]);
+    }
+    for(int i=1;i<=m;i++) { a[i][0]=a[i][n]; a[i][n+1]=a[i][1];}
+    for(int i=1;i<=n;i++) { a[0][i]=a[m][i]; a[m+1][i]=a[1][i]; }
+    for(int i=1;i<=m;i++){
+      for(int j=1;j<=n;j++)
+        System.out.print(a[i-1][j]+a[i+1][j]+a[i][j-1]+a[i][j+1]+" ");
+      System.out.println();
+    }
   }
 }
